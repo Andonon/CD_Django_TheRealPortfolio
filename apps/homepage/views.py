3665e85a7a786d229e1163bc795e02baa3d97227
Troy Center <troycenter1@gmail.com>
2017-06-19 16:16:04 -0700

    pages created, tested, homepage done, css added

--- apps/homepage/views.py
+++ apps/homepage/views.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'homepage/home.html')
+def projects(request):
+    return render(request, 'homepage/projects.html')
+def about(request):
+    return render(request, 'homepage/about.html')
+def testimonials(request):
+    return render(request, 'homepage/testimonials.html')
+
